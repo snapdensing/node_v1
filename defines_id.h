84b431042833e87fb6d74aa2ea295d8ae48f3526
snapdensing <snapdensing@gmail.com>
2019-10-25 17:03:52 +0800

    Added node_id and node_loc to transmit packet

---
+++ defines_id.h
@@ -0,0 +1,11 @@
+/*
+ * defines_id.h
+ *
+ *  Created on: Oct 25, 2019
+ *      Author: Snap
+ */
+
+const char *node_id = "Percival";
+const char *node_loc = "Sapusomo";
+unsigned int node_id_len = 8;
+unsigned int node_loc_len = 8;
