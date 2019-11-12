56c5bf9b4198b3572c6f854898ce1a1196cca415
snapdensing <snapdensing@gmail.com>
2019-11-12 15:32:04 +0800

    defines_id config changed

--- defines_id.h
+++ defines_id.h
@@ -5,9 +5,9 @@
  *      Author: Snap
  */
 
-const char *node_id_default = "Lancelot";
-const char *node_loc_default = "Camelot";
-unsigned int node_id_len_default = 8; // length of node_id string
+const char *node_id_default = "NoName";
+const char *node_loc_default = "Nowhere";
+unsigned int node_id_len_default = 6; // length of node_id string
 unsigned int node_loc_len_default = 7; // length of node_loc string
 
 /* XBee parameter defaults */
