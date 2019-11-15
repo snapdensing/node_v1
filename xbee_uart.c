51c121baf52ca8c46d87ed5996eb187d044573f9
snapdensing <snapdensing@gmail.com>
2019-11-15 16:17:20 +0800

    Added function for catching transmit status. So far, only passes delivery status. Tested said functions on sensing start command

---
+++ xbee_uart.c
@@ -0,0 +1,48 @@
+/*
+ * xbee_uart.c
+ *
+ *  Created on: Nov 15, 2019
+ *      Author: Snap
+ */
+
+#include <msp430.h>
+#include "defines.h"
+//#include "parse_routines.c"
+
+int parse_txstat(char *packet, unsigned int length, char *delivery_p);
+
+int rx_txstat(int *state_p,/* int next_state,*/ int *rxheader_flag_p, unsigned int *rxctr_p, unsigned int *rxpsize_p, char *rxbuf, unsigned int *fail_ctr_p, unsigned int *tx_ctr_p){
+
+    int parsed_txstat, success;
+    char delivery;
+
+    success = 0;
+
+    if (*rxctr_p >= (*rxpsize_p + 4)){
+
+        P3OUT |= 0x40; // UART Rx disable
+
+        parsed_txstat = parse_txstat(rxbuf, *rxpsize_p, &delivery);
+
+        if (parsed_txstat == 1){
+            //*state_p = next_state;
+            *tx_ctr_p = *tx_ctr_p + 1;
+
+            /* Failed transmit ctr */
+            if (delivery != 0x00){
+                *fail_ctr_p = *fail_ctr_p + 1;
+            }else{
+                success = 1;
+            }
+        }
+
+        // Reset buffer
+        *rxctr_p = 0;
+        *rxheader_flag_p = 0;
+        *rxpsize_p = 0;
+        P3OUT &= 0xbf; // UART Rx Enable
+
+    }
+
+    return success;
+}
